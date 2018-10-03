6a4bfb294f0661bcac05c8d205f31cf94ff10d9d
Intrepid Panda <jerick999@gmail.com>
2018-10-03 10:48:03 -0600

    Add testing dependencies

---
+++ src/Card.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Card.css';
+
+class Card extends Component {
+  render() {
+    return (
+      <div>Welcome To Headcount 2.0</div>
+    );
+  }
+}
+
+export default Card;
