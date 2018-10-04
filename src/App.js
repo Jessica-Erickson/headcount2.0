cc753a8f1d31ca1ae63ac2ec2f5a19ba02f434a1
Intrepid Panda <jerick999@gmail.com>
2018-10-04 13:21:36 -0600

    Add all data files to App to use later

--- src/App.js
+++ src/App.js
@@ -4,6 +4,20 @@ import DistrictRepository from './helper';
 import kinderData from './data/kindergartners_in_full_day_program';
 import onlineEnrollment from './data/online_pupil_enrollment';
 import titleIStudents from './data/title_i_students';
+import thirdGradeTests from './data/3rd_grade_tests';
+import eighthGradeTests from './data/8th_grade_test_scores';
+import aveEthMath from './data/average_race_ethnicity_math_scores';
+import aveEthReading from './data/average_race_ethnicity_reading_scores';
+import aveEthWriting from './data/average_race_ethnicity_writing_scores';
+import dropoutRates from './data/dropout_rates_by_race_and_ethnicity';
+import hsGraduation from './data/high_school_graduation_rates';
+import medianIncome from './data/median_household_income';
+import regEnrollment from './data/pupil_enrollment';
+import enrollmentByEth from './data/pupil_enrollment_by_race_ethnicity';
+import remediation from './data/remediation_in_higher_education';
+import childrenInPoverty from './data/school_aged_children_in_poverty';
+import specialEd from './data/special_education';
+import frLunch from './data/students_qualifying_for_free_or_reduced_price_lunch';
 
 class App extends Component {
   constructor() {
