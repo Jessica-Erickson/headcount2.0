48a8c605240272d516c4a779da6e80fec978a59f
Intrepid Panda <jerick999@gmail.com>
2018-10-04 15:47:29 -0600

    Pull in prop types to make sure the Controls gets an array

--- src/Controls.js
+++ src/Controls.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function Controls(props) {
   return (
     <div></div>
     )
+}
+
+Controls.propTypes = {
+  options: PropTypes.array.isRequired
 }
