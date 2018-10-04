2bc78a94f0cede09794174809b7166d9c3590adb
Intrepid Panda <jerick999@gmail.com>
2018-10-03 19:36:17 -0600

    Create App state that keeps track of the search value

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      search: ''
+    }
+  }
+  
   render() {
     return (
       <div>Welcome To Headcount 2.0</div>
