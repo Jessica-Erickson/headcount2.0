41cf888d8d9b42bc7c8e9f13fee574c5eb40720c
Intrepid Panda <jerick999@gmail.com>
2018-10-05 07:39:24 -0600

    Pass in what the Compare components in the test need, update snapshot

--- src/Compare.test.js
+++ src/Compare.test.js
@@ -6,11 +6,18 @@ import {shallow} from 'enzyme';
 describe('Compare', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Compare />, div);
+    ReactDOM.render(<Compare 
+                      toCompare={[]}
+                      repo={{}}
+                      handleCompareClick={() => {}} />, 
+                    div);
   });
 
   it('should match the snapshot', () => {
-    const wrapper = shallow(<Compare />);
+    const wrapper = shallow(<Compare 
+                              toCompare={[]}
+                              repo={{}}
+                              handleCompareClick={() => {}} />);
 
     expect(wrapper).toMatchSnapshot()
   });
