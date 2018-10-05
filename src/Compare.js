4a4a1665d9158dd9fa718d5a7e3e739a3e3d7758
Intrepid Panda <jerick999@gmail.com>
2018-10-05 07:45:48 -0600

    Add in Card files

--- src/Compare.js
+++ src/Compare.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Compare.css';
+import Card from './Card';
 import PropTypes from 'prop-types';
 
 export default function Compare({ toCompare , repo , handleCompareClick }) {
